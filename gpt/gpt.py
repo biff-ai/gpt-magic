57cd9117d42670150887bf99c128e42d35d3ab33
Christophe Carvenius <info@svenskapop.se>
2023-03-25 16:36:22 +0100

    slightly better error handling

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -35,15 +35,18 @@ class GPTMagics(Magics):
             raise Exception(f"Error: {response.status_code}, {response.text}")
 
     def parse_response(self, response_content):
-        response = json.loads(response_content['choices'][0]['message']['content'])
-        explanation = response['explanation']
-        code = response['code']
+        try:
+            response = json.loads(response_content['choices'][0]['message']['content'])
+            explanation = response['explanation']
+            code = response['code']
 
-        # Create a new code cell with the generated code
-        ipython = get_ipython()
-        ipython.set_next_input(code, replace=False)
-        return explanation, code
-
+            # Create a new code cell with the generated code
+            ipython = get_ipython()
+            ipython.set_next_input(code, replace=False)
+            return explanation, code
+        except JSONDecodeError:
+            print(f"Failed to decode: {response_content['choices'][0]['message']['content']}")
+ 
     @line_cell_magic
     def gpt(self, line, cell=None):
         response = self.call_openai_api(line)
