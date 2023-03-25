dc1a833d436e2657a79c1e656738f1e188696d65
Christophe Carvenius <info@svenskapop.se>
2023-03-25 14:41:34 +0100

    Fixed typo

--- gpt_magic/__init__.py
+++ gpt_magic/__init__.py
@@ -7,5 +7,5 @@ def load_ipython_extension(ipython):
     api_key = os.environ.get("OPENAI_API_KEY")
     if not api_key:
         raise ValueError("Please set the OPENAI_API_KEY environment variable.")
-    ipython.register_magics(GPT3Magics(ipython, api_key))
+    ipython.register_magics(GPTMagics(ipython, api_key))
 
