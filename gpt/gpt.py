11072c4e4123b9f0183bad6a123a5599f99c98f7
Jamie Martin <jamiemartin@Jamies-MacBook-Pro.local>
2023-03-30 09:59:58 +0100

    added linting and documetnation to gpt.py

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -9,13 +9,36 @@ from json import JSONDecodeError
 from IPython.display import display, Markdown
 from IPython.core.magic import line_cell_magic, magics_class, Magics
 from IPython.core.getipython import get_ipython
+from typing import Optional
 
 MODEL_STRING = os.getenv('OPENAI_MODEL', 'gpt-3.5-turbo')
 
 @magics_class
 class GPTMagics(Magics):
+    """
+    A set of magics useful for interacting with OpenAI's GPT-3 API.
+
+    You can ask GPT a query. This to be returned in a JSON with explanation and code keys.
+    The explanation is then displayed in markdown, and the code is returned in the cell below.
+    
+    Attributes:
+        api_key (str): The API key for the OpenAI API.
+        prefix_system (str): The system message prefix to use when sending queries to the API.
+        prefix_user (str): The user message prefix to use when sending queries to the API.
+        model (str): The OpenAI GPT model to use for generating responses.
+        temperature (float): The temperature for generating responses from the API.
+        current_query (str): The current query that the user is working on.
+    """
     
     def __init__(self, shell, api_key=None):
+        """
+        Initializes a GPTMagics instance with the specified IPython shell and optional API key.
+        
+        Args:
+            shell (InteractiveShell): The IPython shell for the current session.
+            api_key (str, optional): The API key for the OpenAI API. Defaults to None.
+        """
+
         super().__init__(shell)
         self.api_key = api_key or os.environ['OPENAI_API_KEY']
         openai.api_key = self.api_key
@@ -28,13 +51,33 @@ class GPTMagics(Magics):
         self.current_query = None
     
     ## A chat initialisation if none exists.
-    def init_chat(self, query):
+    def init_chat(self, query: str) -> list:
+        """
+        Initializes a chat if none exists.
+
+        Args:
+            query (str): The user's query to start the chat.
+
+        Returns:
+            list: A list of dictionaries representing the initial chat messages.
+        """
         current_chat = [{"role": "system", "content": f"{self.prefix_system}"},\
                      {"role": "user", "content": f"{self.prefix_user} {query}"}]
         return current_chat
     
     #This method initialises a chat (using function above), if no previous chat exists, otherwise it continues the chat.
-    def cont_chat(self, query, current_chat=None):
+    def cont_chat(self, query: str, current_chat: Optional[list] = None) -> list:
+        """
+        Initializes a chat (using the `init_chat` function) if no previous chat exists,
+        otherwise continues the chat.
+
+        Args:
+            query (str): The user's query to continue the chat.
+            current_chat (list, optional): The current chat log. Defaults to None.
+
+        Returns:
+            list: An updated list of dictionaries representing the chat messages.
+        """
         if current_chat:
             message = {"role": "user", "content": f"{query}"}
             current_chat.append(message)
@@ -42,7 +85,16 @@ class GPTMagics(Magics):
         else:
             return self.init_chat(query)
     
-    def extract_json_object(self, s):
+    def extract_json_object(self, s: str) -> Optional[str]:
+        """
+        Extracts the first JSON object from a given string.
+
+        Args:
+            s (str): The input string to search for a JSON object.
+
+        Returns:
+            Optional[str]: The extracted JSON object as a string, or None if not found.
+        """
         open_braces = 0
         start = -1
         end = -1
@@ -63,7 +115,21 @@ class GPTMagics(Magics):
         else:
             return None
 
-    def parse_response(self, response_content, execute_code=False):
+    def parse_response(self, response_content: str, execute_code: bool = False) -> Tuple[str, str]:
+        """
+        Parses the response from the OpenAI API, extracts the explanation and code, 
+        and inserts the generated code into a new code cell.
+
+        Args:
+            response_content (str): The JSON-formatted response content from the GPT-3 API.
+            execute_code (bool, optional): Whether to execute the generated code. Defaults to False.
+
+        Returns:
+            Tuple[str, str]: A tuple containing the explanation and the code.
+
+        Raises:
+            JSONDecodeError: If the response content cannot be decoded.
+        """
         try:
             response = json.loads(response_content)
             explanation = response['explanation']
@@ -80,7 +146,19 @@ class GPTMagics(Magics):
         except json.JSONDecodeError:
             print(f"Failed to decode: {response_content}")
             
-    def call_openai(self, data):
+    def call_openai(self, data: dict) -> dict:
+        """
+        Sends a request to the OpenAI API with the given data.
+
+        Args:
+            data (dict): The data to be sent as a JSON payload to the OpenAI API.
+
+        Returns:
+            dict: The JSON-formatted response from the OpenAI API.
+
+        Raises:
+            Exception: If the API returns a non-200 status code.
+        """
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.api_key}"
@@ -92,7 +170,16 @@ class GPTMagics(Magics):
         else:
             raise Exception(f"Error: {resp.status_code}, {resp.text}")
         
-    def prepare_payload(self, current_query):
+    def prepare_payload(self, current_query: list) -> dict:
+        """
+        Prepares the payload to be sent to the OpenAI API.
+
+        Args:
+            current_query (list): The current chat log.
+
+        Returns:
+            dict: A dictionary containing the prepared payload data.
+        """
         payload_data = {
         'model': self.model,
         'temperature': self.temperature,
@@ -100,8 +187,14 @@ class GPTMagics(Magics):
         }
         return payload_data
     
-    def run(self, query, chat_memory=False):
-        
+    def run(self, query: str, chat_memory: bool = False) -> None:
+        """
+        Assembles the payload, runs the OpenAI API request and processes the response.
+
+        Args:
+            query (str): The user's query to send to the GPT-3 API.
+            chat_memory (bool, optional): Whether to include chat memory. Defaults to False.
+        """
         #Includes the chat memory if set to true (if argument is included).
         if not chat_memory:
             self.current_query = None
@@ -122,19 +215,33 @@ class GPTMagics(Magics):
         
     
     @line_cell_magic
-    def gpt(self, line, cell=None):
+    def gpt(self, line: str, cell: Optional[str] = None) -> None:
+        """
+        IPython magic function that processes user input and runs the OPENAI API request.
+
+        Args:
+            line (str): The line input provided by the user.
+            cell (str, optional): The cell input provided by the user. Defaults to None.
+        """
+
+        # Create an argument parser for processing command-line options
         parser = argparse.ArgumentParser()
         parser.add_argument("-c", action="store_true")
         parser.add_argument("-t", type=float, default=0)
         parser.add_argument("query", nargs=argparse.REMAINDER)
         args = parser.parse_args(line.split())
-
+        
+        # Determine whether chat memory should be enabled
         chat_memory = args.c
+        
+        # Combine the remainder of the arguments into a single query string
         query = " ".join(args.query)
-
+        
+        # Set the temperature if it's within the valid range (0 <= t <= 1)
         if 0 <= args.t <= 1:
             self.temperature = args.t
         else:
             raise ValueError("Temperature must be between 0 and 1.")
-
+        
+        # Run the OpenAI API request with the provided query and chat memory option
         self.run(query, chat_memory)
